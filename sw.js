a7d4814ea4bdb94c528f0fed50886bda593d5bfc
rohithkd <kdrohith@gmail.com>
2018-12-12 23:54:53 +0530

    Service worker

---
+++ sw.js
@@ -0,0 +1,35 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/?homescreen=1',
+       '/css/intlTelInput.css',
+       '/css/bootstrap.min.css',
+       'css/custom.css',
+       'js/jquery.js',
+       'js/bootstrap.min.js'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+
+console.log(event.request.url);
+
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
