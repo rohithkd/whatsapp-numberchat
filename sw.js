253fe880f730cae5905a45d1b4582b01bbeea370
rohithkd <kdrohith@gmail.com>
2018-12-13 00:08:47 +0530

    Fixngservice worker

--- sw.js
+++ sw.js
@@ -1,35 +1,45 @@
-importScripts('/cache-polyfill.js');
-
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
+var dataCacheName = 'template-pwa';
+var cacheName = 'template-pwa';
+var filesToCache = [
        '/',
        '/index.html',
-       '/?homescreen=1',
        '/css/intlTelInput.css',
        '/css/bootstrap.min.css',
        'css/custom.css',
        'js/jquery.js',
        'js/bootstrap.min.js'
-     ]);
-   })
- );
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
 
-console.log(event.request.url);
-
-event.respondWith(
-
-caches.match(event.request).then(function(response) {
-
-return response || fetch(event.request);
-
-})
-
-);
-
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
